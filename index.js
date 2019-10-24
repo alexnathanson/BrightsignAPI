4ec937d856d2bcc6db4f23542e7eafedadba11c1
alexnathanson <alex@alexnathanson.com>
2019-10-24 17:31:11 -0400

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+//change directory
+let process = require("process");
+process.chdir("/storage/sd");
+
+
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+var device_info = new BSDeviceInfo();
+response.writeHead(200, {"Content-Type": "text/plain"});
+response.end("Device Information:\n" + device_info.model + "\n" + device_info.bootVersion + "\n");
+var ip = request.connection.remoteAddress;
+document.getElementById("Ip").innerHTML+="Server responded to: "+ ip + "<br>";
+console.log("Server responded to request from " + ip);
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Display it on brightsign browser
+var os = require('os');
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+  for (var k2 in interfaces[k]) {
+      var address = interfaces[k][k2];
+      if (address.family === 'IPv4' && !address.internal) {
+          addresses.push(address.address);
+      }
+  }
+}
+
+var message = "Server running at: " + addresses[0] + ":8000<br>";
+document.getElementById("Ip").innerHTML+= message;
+
+// Print message on console
+console.log(message);
